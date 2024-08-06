570c44eec210084102e81802ea3fa4b381f5efdb
Daniyal Pitafi <daniyalpitafi21@gmail.com>
2024-08-06 10:22:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes in the app
 
-# Path to the JSON file where data will be saved
+# Path to the JSON file where data will be saved mamooon
 json_file_path = os.path.join(os.path.dirname(__file__), 'data.json')
 
 # Function to load existing data from the JSON file
